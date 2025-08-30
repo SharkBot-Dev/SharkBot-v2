35c6563e8e99f28c13e2522271d7ecf586522f79
sharkbot-neko <modoggu12345@outlook.com>
2025-08-30 22:13:38 +0900

    お金がないのに引ける問題を修正

--- cogs/money.py
+++ cogs/money.py
@@ -212,6 +212,12 @@ class GachaGroup(app_commands.Group):
         
         await interaction.response.defer()
 
+        m = await Money(interaction.client).get_server_money(
+            interaction.guild, interaction.user
+        )
+        if m < dbfind["Money"]:
+            return await interaction.response.send_message(ephemeral=True, embed=discord.Embed(title="ガチャを引くためのお金がありません。", color=discord.Color.red()))
+
         await Money(interaction.client).add_server_money(interaction.guild, interaction.user, -dbfind.get("Money", 1))
 
         ch = random.choice(dbfind.get("Item", []))
