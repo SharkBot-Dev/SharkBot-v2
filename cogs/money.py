3c8eb26756e2bbdce0d894712c3cbeb70a6e000e
sharkbot-neko <modoggu12345@outlook.com>
2025-09-01 17:39:28 +0900

    バグ修正3

--- cogs/money.py
+++ cogs/money.py
@@ -372,17 +372,19 @@ class GachaGroup(app_commands.Group):
 
         await interaction.response.defer()
 
-        m = await Money(interaction.client).get_server_money(
-            interaction.guild, interaction.user
-        )
-        if m <= dbfind["Money"]:
-            return await interaction.followup.send(
-                ephemeral=True,
-                embed=discord.Embed(
-                    title="ガチャを引くためのお金がありません。",
-                    color=discord.Color.red(),
-                ),
+        if dbfind["Money"] != 0:
+
+            m = await Money(interaction.client).get_server_money(
+                interaction.guild, interaction.user
             )
+            if m < dbfind["Money"]:
+                return await interaction.followup.send(
+                    ephemeral=True,
+                    embed=discord.Embed(
+                        title="ガチャを引くためのお金がありません。",
+                        color=discord.Color.red(),
+                    ),
+                )
 
         await Money(interaction.client).add_server_money(
             interaction.guild, interaction.user, -dbfind.get("Money", 1)
