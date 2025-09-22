5595beccbb6e8a768fca5375115d4f13ccfe5bad
sharkbot-neko <modoggu12345@outlook.com>
2025-09-22 20:14:39 +0900

    便利な関数を追加

---
+++ models/cooldown.py
@@ -0,0 +1,22 @@
+import time
+
+cooldowns = {}
+
+class Cooldown:
+    def __init__(self, cooldown_time: int):
+        self.cooldown_time = cooldown_time
+
+    def check(self, id: str):
+        now = time.time()
+        last_used = cooldowns.get(id)
+
+        if last_used is not None and (now - last_used) < self.cooldown_time:
+            remaining = self.cooldown_time - (now - last_used)
+            return remaining
+
+        cooldowns[id] = now
+        return None
+
+    def clear(self, id: str):
+        if id in cooldowns:
+            del cooldowns[id]
---
+++ models/string_id.py
@@ -0,0 +1,5 @@
+import random, string
+
+def string_id(length: int):
+    randlst = [random.choice(string.ascii_letters + string.digits) for i in range(length)]
+    return ''.join(randlst)
