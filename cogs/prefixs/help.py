1619ea98c0fbab7998162fd2fad811b6c344f44d
sharkbot-neko <modoggu12345@outlook.com>
2025-10-12 19:37:53 +0900

    prefixコマンドを少々修正

---
+++ cogs/prefixs/help.py
@@ -0,0 +1,63 @@
+from discord.ext import commands
+import discord
+from consts import settings
+from discord import app_commands
+from models import command_disable, make_embed
+
+class Paginator(discord.ui.View):
+    def __init__(self, embeds: list[discord.Embed]):
+        super().__init__(timeout=60)
+        self.embeds = embeds
+        self.current = 0
+
+    async def update_message(self, interaction: discord.Interaction):
+        await interaction.response.edit_message(
+            embed=self.embeds[self.current], view=self
+        )
+
+    @discord.ui.button(label="⬅️", style=discord.ButtonStyle.primary)
+    async def previous(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        self.current = (self.current - 1) % len(self.embeds)
+        await self.update_message(interaction)
+
+    @discord.ui.button(label="➡️", style=discord.ButtonStyle.primary)
+    async def next(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.current = (self.current + 1) % len(self.embeds)
+        await self.update_message(interaction)
+
+class Prefixs_HelpCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        print("init -> Prefixs_HelpCog")
+
+    @commands.command(name="help", aliases=["h"], hidden=True)
+    @commands.cooldown(2, 5, type=commands.BucketType.guild)
+    async def help_prefix(self, ctx: commands.Context):
+        if not await command_disable.command_enabled_check_by_cmdname("help", ctx.guild):
+            return
+        
+        ems = []
+
+        for start in range(0, len(list(self.bot.commands)), 10):
+            embed = make_embed.success_embed(title="SharkBotのヘルプ (頭文字バージョン)", description="頭文字バージョンです。\nスラッシュコマンド用ヘルプは、\n`/help`を使用してください。\n\nちなみに、頭文字コマンドは、\n`[頭文字]コマンド名`と送信することで機能します。\n\n標準頭文字: `!.`")
+            for cmd in list(self.bot.commands)[start : start + 10]:
+                if "load" in cmd.name:
+                    continue
+                if "jishaku" in cmd.name:
+                    continue
+                if "sync_slash" == cmd.name:
+                    continue
+                if "save" == cmd.name:
+                    continue
+
+                al = ', '.join(cmd.aliases)
+                embed.add_field(name=cmd.name, value=cmd.description + f"\n別名: " + al if al else "なし", inline=False)
+
+            ems.append(embed)
+
+        await ctx.reply(embed=ems[0], view=Paginator(ems))
+
+async def setup(bot):
+    await bot.add_cog(Prefixs_HelpCog(bot))
