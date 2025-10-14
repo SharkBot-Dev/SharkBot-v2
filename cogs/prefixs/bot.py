6bef3c2b6d4ab4d573e06134bdd900f12125f0c5
sharkbot-neko <modoggu12345@outlook.com>
2025-10-14 22:49:19 +0900

    Pingコマンドの追加

---
+++ cogs/prefixs/bot.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+import discord
+from models import command_disable, make_embed
+
+class Prefixs_BotCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        print("init -> Prefixs_BotCog")
+
+    @commands.command(name="ping", description="Pingを測定します。")
+    @commands.cooldown(2, 5, type=commands.BucketType.guild)
+    async def ping_prefix(self, ctx: commands.Context):
+        if not await command_disable.command_enabled_check_by_cmdname("bot ping", ctx.guild):
+            return
+        
+        embed = make_embed.success_embed(title='Pingを測定しました。', description=f"DiscordAPI: {round(self.bot.latency * 1000)}ms")
+        await ctx.reply(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Prefixs_BotCog(bot))
