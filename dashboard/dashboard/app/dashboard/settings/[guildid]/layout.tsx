8e0c010c0e943ea668e91e6b7fd9844c767987b6
sharkbot-neko <modoggu12345@outlook.com>
2025-11-03 12:48:21 +0900

    絵文字を追加

--- dashboard/dashboard/app/dashboard/settings/[guildid]/layout.tsx
+++ dashboard/dashboard/app/dashboard/settings/[guildid]/layout.tsx
@@ -25,34 +25,34 @@ export default async function GuildLayout({
 
         <nav className="space-y-2 mt-6">
           <a href={`/dashboard/`} className="block p-2 rounded hover:bg-gray-700 text-gray-200">
-            サーバー選択
+            🦈サーバー選択
           </a>
           <a href={`/dashboard/settings/${guildid}`} className="block p-2 rounded hover:bg-gray-700 text-gray-200">
-            ホーム
+            🏠ホーム
           </a>
           <a href={`/dashboard/settings/${guildid}/commands`} className="block p-2 rounded hover:bg-gray-700 text-gray-200">
-            コマンド一覧
+            💬コマンド一覧
           </a>
           <a href={`/dashboard/settings/${guildid}/join-message`} className="block p-2 rounded hover:bg-gray-700 text-gray-200">
-            よろしくメッセージ
+            🤝よろしくメッセージ
           </a>
           <a href={`/dashboard/settings/${guildid}/leave-message`} className="block p-2 rounded hover:bg-gray-700 text-gray-200">
-            さようならメッセージ
+            👋さようならメッセージ
           </a>
           <a href={`/dashboard/settings/${guildid}/auto-thread`} className="block p-2 rounded hover:bg-gray-700 text-gray-200">
-            自動スレッド作成
+            📖自動スレッド作成
           </a>
           <a href={`/dashboard/settings/${guildid}/expand`} className="block p-2 rounded hover:bg-gray-700 text-gray-200">
-            メッセージ展開
+            💬メッセージ展開
           </a>
           <a href={`/dashboard/settings/${guildid}/poll`} className="block p-2 rounded hover:bg-gray-700 text-gray-200">
-            投票
+            🗳️投票
           </a>
           <a href={`/dashboard/settings/${guildid}/level`} className="block p-2 rounded hover:bg-gray-700 text-gray-200">
-            レベル
+            🏆レベル
           </a>
           <a href={`/dashboard/settings/${guildid}/economy`} className="block p-2 rounded hover:bg-gray-700 text-gray-200">
-            サーバー内経済
+            🪙サーバー内経済
           </a>
         </nav>
       </aside>
